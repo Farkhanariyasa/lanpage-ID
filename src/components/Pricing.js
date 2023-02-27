415a210e31c1599aca84ff7f7cfe690c16b502c1
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-02-27 20:38:00 +0700

    Adding tab to pricing

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import PricingKonsultasi from "./PricingKonsultasi";
+import PricingTutor from "./PricingTutor";
 
 const Pricing = () => {
+  const [isTutor, setIsTutor] = useState("konsultasi");
+
+  const konsultasiHandler = () => {
+    setIsTutor("konsultasi");
+  };
+
+  const tutorHandler = () => {
+    setIsTutor("tutor");
+  };
+
   return (
-    <div data-aos="fade-down" data-aos-delay="600">
-      <div class="min-h-screen pb-28 sm:pb-0">
+    <div data-aos="fade-down" data-aos-delay="200">
+      <div class="min-h-[1100px] pb-28 sm:pb-0">
         <div class="lg:h-[767px]">
           <div class="max-w-7xl min-h-screen mx-auto p-4 sm:p-10 lg:py-20">
             <div class="max-w-5xl flex-col justify-center items-center text-center mx-auto bg  text-white tracking-widest pb-10 lg:pb-20">
               <h1 class="h2 mb-6 xl:mb-12">
-                The Right Price for You, Whoeveryou Are
+                The Right Price for You, Whoeveryou re
               </h1>
               <p class="lead text-center ">
                 Lorem ipsum has been the industry's standard dummy text ever
@@ -16,292 +28,31 @@ const Pricing = () => {
                 scrambled.
               </p>
             </div>
-
-            <div class="flex flex-col lg:flex-row justify-center items-center  gap-8 lg:gap-0 lg:mt-4">
-              <div class="flex-1 w-full mb-6 bg-white rounded-xl shadow-xl lg:scale-95">
-                <div class="text-center p-12">
-                  <p class="text-3xl lg:text-2xl xl:text-3xl pb-4">Hobby</p>
-                  <div class="flex justify justify-center items-center">
-                    <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
-                      $ 79
-                    </span>
-                    <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
-                      /month
-                    </span>
-                  </div>
-                </div>
-                <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
-                  <ul class="space-y-4">
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        when an unknown printer took a galley.
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        leap into electronic typesetting remaining.{" "}
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        ontaining Lorem Ipsum passages and more.
-                      </span>
-                    </li>
-                  </ul>
+            <div class="max-w-xl flex-col justify-center items-center text-center mx-auto bg  text-white tracking-widest pb-10 lg:pb-20">
+              <ul class="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg shadow sm:flex">
+                <li class="w-full">
                   <button
-                    type="button"
-                    class="w-full text-center bg-white text-lg text-indigo-600 mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-600"
+                    onClick={konsultasiHandler}
+                    class="inline-block w-full p-4 bg-white hover:text-gray-700  rounded-r-lg hover:bg-gray-50 focus:ring-2 focus:ring-blue-300 focus:outline-none"
                   >
-                    Start your trial
+                    Konsultasi Statistik
                   </button>
-                </div>
-              </div>
-              <div class="lg:max-w-max mb-6 relative w-full bg-white rounded-xl shadow-xl border-2 border-indigo-600 lg:scale-110 z-10">
-                <div class="text-center p-12">
-                  <p class="text-3xl lg:text-2xl xl:text-3xl pb-4 font-semibold">
-                    Growth
-                  </p>
-                  <div class="flex justify justify-center items-center">
-                    <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
-                      $ 149
-                    </span>
-                    <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
-                      /month
-                    </span>
-                  </div>
-                </div>
-                <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
-                  <ul class="space-y-4">
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        when an unknown printer took a galley.
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        leap into electronic typesetting remaining.{" "}
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        ontaining Lorem Ipsum passages and more.
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        leap into electronic typesetting remaining.{" "}
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        ontaining Lorem Ipsum passages and more.
-                      </span>
-                    </li>
-                  </ul>
+                </li>
+                <li class="w-full">
                   <button
-                    type="button"
-                    class="w-full text-center bg-indigo-600 text-lg text-white mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-700"
+                    onClick={tutorHandler}
+                    class="inline-block w-full p-4 bg-white hover:text-gray-700  rounded-r-lg hover:bg-gray-50 focus:ring-2 focus:ring-blue-300 focus:outline-none"
                   >
-                    Start your trial
+                    Tutor Statistik
                   </button>
-                </div>
-                <div class="absolute rounded-full w-40 bg-indigo-600 text-white text-center text-sm tracking-wider px-4 py-1 -top-3 inset-x-0 mx-auto">
-                  MOST POPULAR
-                </div>
-              </div>
-              <div class="flex-1 w-full mb-6 bg-white rounded-xl shadow-xl lg:scale-95">
-                <div class="text-center p-12">
-                  <p class="text-3xl lg:text-2xl xl:text-3xl pb-4">Hobby</p>
-                  <div class="flex justify justify-center items-center">
-                    <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
-                      $ 349
-                    </span>
-                    <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
-                      /month
-                    </span>
-                  </div>
-                </div>
-                <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
-                  <ul class="space-y-4">
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        when an unknown printer took a galley.
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        leap into electronic typesetting remaining.{" "}
-                      </span>
-                    </li>
-                    <li class="flex">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 mr-3 text-green-500"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
-                          d="M5 13l4 4L19 7"
-                        />
-                      </svg>
-                      <span class="text-gray-500">
-                        ontaining Lorem Ipsum passages and more.
-                      </span>
-                    </li>
-                  </ul>
-                  <button
-                    type="button"
-                    class="w-full text-center bg-white text-lg text-indigo-600 mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-600"
-                  >
-                    Start your trial
-                  </button>
-                </div>
-              </div>
+                </li>
+              </ul>
             </div>
+            {isTutor === "konsultasi" ? (
+              <PricingKonsultasi />
+            ) : (
+              <PricingTutor />
+            )}
           </div>
         </div>
       </div>
---
+++ src/components/PricingTutor.js
@@ -0,0 +1,295 @@
+import React from "react";
+
+const PricingTutor = () => {
+  return (
+    <div data-aos="fade-down" data-aos-delay="200">
+      <div class="flex flex-col lg:flex-row justify-center items-center  gap-8 lg:gap-0 lg:mt-4">
+        <div class="flex-1 w-full mb-6 bg-white rounded-xl shadow-xl lg:scale-95">
+          <div class="text-center p-12">
+            <p class="text-3xl lg:text-2xl xl:text-3xl pb-4">Hobby</p>
+            <div class="flex justify justify-center items-center">
+              <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
+                $ 79
+              </span>
+              <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
+                /month
+              </span>
+            </div>
+          </div>
+          <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
+            <ul class="space-y-4">
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  when an unknown printer took a galley.
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  leap into electronic typesetting remaining.{" "}
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  ontaining Lorem Ipsum passages and more.
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              class="w-full text-center bg-white text-lg text-indigo-600 mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-600"
+            >
+              Start your trial
+            </button>
+          </div>
+        </div>
+        <div class="lg:max-w-max mb-6 relative w-full bg-white rounded-xl shadow-xl border-2 border-indigo-600 lg:scale-110 z-10">
+          <div class="text-center p-12">
+            <p class="text-3xl lg:text-2xl xl:text-3xl pb-4 font-semibold">
+              Growth
+            </p>
+            <div class="flex justify justify-center items-center">
+              <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
+                $ 149
+              </span>
+              <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
+                /month
+              </span>
+            </div>
+          </div>
+          <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
+            <ul class="space-y-4">
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  when an unknown printer took a galley.
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  leap into electronic typesetting remaining.{" "}
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  ontaining Lorem Ipsum passages and more.
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  leap into electronic typesetting remaining.{" "}
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  ontaining Lorem Ipsum passages and more.
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              class="w-full text-center bg-indigo-600 text-lg text-white mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-700"
+            >
+              Start your trial
+            </button>
+          </div>
+          <div class="absolute rounded-full w-40 bg-indigo-600 text-white text-center text-sm tracking-wider px-4 py-1 -top-3 inset-x-0 mx-auto">
+            MOST POPULAR
+          </div>
+        </div>
+        <div class="flex-1 w-full mb-6 bg-white rounded-xl shadow-xl lg:scale-95">
+          <div class="text-center p-12">
+            <p class="text-3xl lg:text-2xl xl:text-3xl pb-4">Hobby</p>
+            <div class="flex justify justify-center items-center">
+              <span class="font-extrabold text-5xl lg:text-4xl xl:text-6xl align-text-middle px-3">
+                $ 349
+              </span>
+              <span class="font-normal text-xl text-gray-500 inline-block align-text-middle">
+                /month
+              </span>
+            </div>
+          </div>
+          <div class="bg-gray-100 rounded-b-xl border-t-2 border-gray-200/20 p-10">
+            <ul class="space-y-4">
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  when an unknown printer took a galley.
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  leap into electronic typesetting remaining.{" "}
+                </span>
+              </li>
+              <li class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-6 w-6 mr-3 text-green-500"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M5 13l4 4L19 7"
+                  />
+                </svg>
+                <span class="text-gray-500">
+                  ontaining Lorem Ipsum passages and more.
+                </span>
+              </li>
+            </ul>
+            <button
+              type="button"
+              class="w-full text-center bg-white text-lg text-indigo-600 mt-8 p-3 rounded shadow-xl transition hover:text-white hover:bg-indigo-600"
+            >
+              Start your trial
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PricingTutor;
