1c2fb6406db7ed94fe5dc33385dca9c863c95de3
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-03-09 21:51:27 +0700

    styling faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -23,7 +23,11 @@ const FAQ = () => {
                   </summary>
 
                   <span>
-                    Normally,  pengerjaan olah data bisa selesai dalam waktu 2-4 hari karena ada antrian. Tapi kalau kamu urgent banget harus kelar besoknya, kita bisa kok siapin paket express buat kamu. Kamu cuma nambah 20% aja dari harga yang sudah disepakati. 
+                    Normally, pengerjaan olah data bisa selesai dalam waktu 2-4
+                    hari karena ada antrian. Tapi kalau kamu urgent banget harus
+                    kelar besoknya, kita bisa kok siapin paket express buat
+                    kamu. Kamu cuma nambah 20% aja dari harga yang sudah
+                    disepakati.
                   </span>
                 </details>
                 <details class="mb-4">
@@ -32,7 +36,9 @@ const FAQ = () => {
                   </summary>
 
                   <span>
-                    Pastinya!!! Tim kami dilead oleh statistician lulusan Amerika Serikat yang udah biasa menghadapi klien ga cuma dari Indonesia tapi juga dari luar negeri loh!
+                    Pastinya!!! Tim kami dilead oleh statistician lulusan
+                    Amerika Serikat yang udah biasa menghadapi klien ga cuma
+                    dari Indonesia tapi juga dari luar negeri loh!
                   </span>
                 </details>
                 <details class="mb-4">
@@ -41,7 +47,10 @@ const FAQ = () => {
                   </summary>
 
                   <span>
-                    Bisa donk! Jadi setelah teman-teman mengirimkan datanya, dalam waktu 2-4 hari, teman-teman akan kami ajak meeting untuk kami ajarkan cara pengolahan datanya dari awal sampai bagaimana cara membacanya!
+                    Bisa donk! Jadi setelah teman-teman mengirimkan datanya,
+                    dalam waktu 2-4 hari, teman-teman akan kami ajak meeting
+                    untuk kami ajarkan cara pengolahan datanya dari awal sampai
+                    bagaimana cara membacanya!
                   </span>
                 </details>
                 <details class="mb-4">
@@ -50,7 +59,11 @@ const FAQ = () => {
                   </summary>
 
                   <span>
-                  Kita akan bantu menemukan metode yang tepat sesuai dengan kondisi data kamu. Kami tidak akan memanipulasi data alias ngubah-ngubah data apapun karena kan ga boleh ya… Instead, kami akan bantu mencarikan justifikasi yang bisa kamu pakai buat menghadapi sidang.
+                    Kita akan bantu menemukan metode yang tepat sesuai dengan
+                    kondisi data kamu. Kami tidak akan memanipulasi data alias
+                    ngubah-ngubah data apapun karena kan ga boleh ya… Instead,
+                    kami akan bantu mencarikan justifikasi yang bisa kamu pakai
+                    buat menghadapi sidang.
                   </span>
                 </details>
                 <details class="mb-4">
@@ -59,7 +72,10 @@ const FAQ = () => {
                   </summary>
 
                   <span>
-                    Temen-temen dapat GRATIS revisi minor kok. Kecuali kalau berubah total dari awal (bahkan data dan tujuan penelitiannya ganti), nanti ada charge 50% dari harga yang sudah disepakati.
+                    Temen-temen dapat GRATIS revisi minor kok. Kecuali kalau
+                    berubah total dari awal (bahkan data dan tujuan
+                    penelitiannya ganti), nanti ada charge 50% dari harga yang
+                    sudah disepakati.
                   </span>
                 </details>
               </div>
@@ -69,8 +85,10 @@ const FAQ = () => {
                     Firstat bisa melayani aplikasi statistik apa aja?
                   </summary>
 
-                  <span class="px-4 py-2">
-                    Kita bisa bantu analisa data kamu dengan program statistik apa aja, mulai dari SPSS, R, Python, SmartPLS, MPlus, EVIEWS, SAS, STATA.
+                  <span >
+                    Kita bisa bantu analisa data kamu dengan program statistik
+                    apa aja, mulai dari SPSS, R, Python, SmartPLS, MPlus,
+                    EVIEWS, SAS, STATA.
                   </span>
                 </details>
                 <details class="mb-4">
@@ -78,33 +96,63 @@ const FAQ = () => {
                     Bisa ketemu offline nggak?
                   </summary>
 
-                  <span class="px-4 py-2">
-                    Ooo bisa dong!!! hubungin aja dulu, kalo Tuhan mengizinkan kita bisa ketemu kok di mana pun kamu berada. Soalnya, tim konsultan kami tersebar di berbagai kota di Indonesia.
+                  <span >
+                    Ooo bisa dong!!! hubungin aja dulu, kalo Tuhan mengizinkan
+                    kita bisa ketemu kok di mana pun kamu berada. Soalnya, tim
+                    konsultan kami tersebar di berbagai kota di Indonesia.
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    Cara pemesanannya gimana? 
+                    Cara pemesanannya gimana?
                   </summary>
 
-                  <span class="px-4 py-2">
-                    1. Hubungi kontak kami melalui WA, telepon, atau email yang sudah tertera
-                    2. Free konsultasi untuk mengetahui apakah projek kamu memungkinkan untuk kami kerjakan
-                    3. Kamu akan diminta untuk mengirimkan bab 1,2 dan 3 atau setidaknya hipotesis penelitian serta data set
-                    4. Kamu melakukan 50% pembayaran 
-                    5. Proses pengerjaan analisis akan memakan waktu selama 1 - 3 hari
-                    6. Hasil analisis akan disampaikan atau didiskusikan melalui meeting (online/offline)
-                    7. Kamu melakukan sisa 50% pembayaran 
-                    8. Ketika pembayaran sudah diterima, hasil, laporan dan lampiran akan dikirimkan melalui email
+                  <span >
+                    <div class="flex flex-col space-y-4">
+                      <p class="text-lg font-medium">
+                        Cara Memulai Projek Analisis Data
+                      </p>
+                      <ol class="list-decimal list-inside space-y-2">
+                        <li>
+                          Hubungi kontak kami melalui WA, telepon, atau email
+                          yang sudah tertera
+                        </li>
+                        <li>
+                          Free konsultasi untuk mengetahui apakah projek kamu
+                          memungkinkan untuk kami kerjakan
+                        </li>
+                        <li>
+                          Kamu akan diminta untuk mengirimkan bab 1,2 dan 3 atau
+                          setidaknya hipotesis penelitian serta data set
+                        </li>
+                        <li>Kamu melakukan 50% pembayaran</li>
+                        <li>
+                          Proses pengerjaan analisis akan memakan waktu selama 1
+                          - 3 hari
+                        </li>
+                        <li>
+                          Hasil analisis akan disampaikan atau didiskusikan
+                          melalui meeting (online/offline)
+                        </li>
+                        <li>Kamu melakukan sisa 50% pembayaran</li>
+                        <li>
+                          Ketika pembayaran sudah diterima, hasil, laporan dan
+                          lampiran akan dikirimkan melalui email
+                        </li>
+                      </ol>
+                    </div>
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    Metode pembayarannya gimana? 
+                    Metode pembayarannya gimana?
                   </summary>
 
-                  <span class="px-4 py-2">
-                    Untuk saat ini kami hanya menggunakan transfer bank untuk metode pembayaran. Kamu boleh bayar 50% dulu di awal ketika kita sudah deal, dan 50% sisanya bisa kamu bayarkan setelah dapat hasilnya.
+                  <span >
+                    Untuk saat ini kami hanya menggunakan transfer bank untuk
+                    metode pembayaran. Kamu boleh bayar 50% dulu di awal ketika
+                    kita sudah deal, dan 50% sisanya bisa kamu bayarkan setelah
+                    dapat hasilnya.
                   </span>
                 </details>
               </div>
