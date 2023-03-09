75366b068030c1f5672b7f24ea2c79b333b1b0da
Drajat_Ali <222011746@stis.ac.id>
2023-03-09 21:37:26 +0700

    input data faq

--- src/components/FAQ.js
+++ src/components/FAQ.js
@@ -19,70 +19,92 @@ const FAQ = () => {
               <div class="w-full lg:w-1/2 px-4 py-2">
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How Long is this site live?
+                    Pengerjaannya berapa hari?
                   </summary>
 
                   <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    Normally,  pengerjaan olah data bisa selesai dalam waktu 2-4 hari karena ada antrian. Tapi kalau kamu urgent banget harus kelar besoknya, kita bisa kok siapin paket express buat kamu. Kamu cuma nambah 20% aja dari harga yang sudah disepakati. 
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">
-                    Can I install/upload anything I want on there?
+                    Hasil olah datanya beneran akurat?
                   </summary>
 
                   <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    Pastinya!!! Tim kami dilead oleh statistician lulusan Amerika Serikat yang udah biasa menghadapi klien ga cuma dari Indonesia tapi juga dari luar negeri loh!
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How can I migrate to another site?
+                    Nanti diajarin nggak cara ngolah datanya?
                   </summary>
 
                   <span>
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    Bisa donk! Jadi setelah teman-teman mengirimkan datanya, dalam waktu 2-4 hari, teman-teman akan kami ajak meeting untuk kami ajarkan cara pengolahan datanya dari awal sampai bagaimana cara membacanya!
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                    Kalo hasilnya nggak sesuai harapan gimana?
+                  </summary>
+
+                  <span>
+                  Kita akan bantu menemukan metode yang tepat sesuai dengan kondisi data kamu. Kami tidak akan memanipulasi data alias ngubah-ngubah data apapun karena kan ga boleh ya… Instead, kami akan bantu mencarikan justifikasi yang bisa kamu pakai buat menghadapi sidang.
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                    Kalo ada revisi gimana? Bayar lagi nggak?
+                  </summary>
+
+                  <span>
+                    Temen-temen dapat GRATIS revisi minor kok. Kecuali kalau berubah total dari awal (bahkan data dan tujuan penelitiannya ganti), nanti ada charge 50% dari harga yang sudah disepakati.
                   </span>
                 </details>
               </div>
               <div class="w-full lg:w-1/2 px-4 py-2">
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    Can I change the domain you give me?
+                    Firstat bisa melayani aplikasi statistik apa aja?
                   </summary>
 
                   <span class="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    Kita bisa bantu analisa data kamu dengan program statistik apa aja, mulai dari SPSS, R, Python, SmartPLS, MPlus, EVIEWS, SAS, STATA.
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How many sites I can create at once?
+                    Bisa ketemu offline nggak?
                   </summary>
 
                   <span class="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    Ooo bisa dong!!! hubungin aja dulu, kalo Tuhan mengizinkan kita bisa ketemu kok di mana pun kamu berada. Soalnya, tim konsultan kami tersebar di berbagai kota di Indonesia.
                   </span>
                 </details>
                 <details class="mb-4">
                   <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
-                    How can I communicate with you?
+                    Cara pemesanannya gimana? 
                   </summary>
 
                   <span class="px-4 py-2">
-                    Laboris qui labore cillum culpa in sunt quis sint veniam.
-                    Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis
-                    minim velit nostrud pariatur culpa magna in aute.
+                    1. Hubungi kontak kami melalui WA, telepon, atau email yang sudah tertera
+                    2. Free konsultasi untuk mengetahui apakah projek kamu memungkinkan untuk kami kerjakan
+                    3. Kamu akan diminta untuk mengirimkan bab 1,2 dan 3 atau setidaknya hipotesis penelitian serta data set
+                    4. Kamu melakukan 50% pembayaran 
+                    5. Proses pengerjaan analisis akan memakan waktu selama 1 - 3 hari
+                    6. Hasil analisis akan disampaikan atau didiskusikan melalui meeting (online/offline)
+                    7. Kamu melakukan sisa 50% pembayaran 
+                    8. Ketika pembayaran sudah diterima, hasil, laporan dan lampiran akan dikirimkan melalui email
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">
+                    Metode pembayarannya gimana? 
+                  </summary>
+
+                  <span class="px-4 py-2">
+                    Untuk saat ini kami hanya menggunakan transfer bank untuk metode pembayaran. Kamu boleh bayar 50% dulu di awal ketika kita sudah deal, dan 50% sisanya bisa kamu bayarkan setelah dapat hasilnya.
                   </span>
                 </details>
               </div>
