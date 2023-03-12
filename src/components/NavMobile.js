e5e258288a87db8b08ef24b6db79ff2fc98a1813
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-03-12 22:17:02 +0700

    Arrange nav and header

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -5,7 +5,7 @@ import { navigationData } from '../data';
 
 const NavMobile = () => {
   return (
-    <ul className='flex flex-col px-6 py-8 gap-y-4'>
+    <ul className='flex flex-col px-6 py-8 h-full gap-y-4'>
       {navigationData.map((item, index) => {
         return (
           <li key={index}>
