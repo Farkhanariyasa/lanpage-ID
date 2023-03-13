e28707ae6fc847e5a11b6c88cfdccc93de824be4
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-03-13 10:35:13 +0700

    add about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,36 +1,37 @@
-import React from 'react';
+import React from "react";
 
 // import about data
-import { aboutData } from '../data';
+import { aboutData } from "../data";
 
 const About = () => {
   // destructure about
-  const { image, title, subtitle } = aboutData;
+  const { image, title, subtitle1, subtitle2, subtitle3, subtitle4 } =
+    aboutData;
   return (
     <section
-      className='my-[30px] xl:mt-[100px]'
-      data-aos='fade-up'
-      data-aos-offset='350'
+      className="my-[30px] xl:mt-[100px]"
+      data-aos="fade-up"
+      data-aos-offset="350"
     >
-      <div className='container mx-auto'>
-        <div className='bg-accent-secondary/[25%] rounded-[50px] min-h-[560px] px-12 pb-12 flex flex-col text-center xl:flex-row xl:items-center xl:text-left  xl:gap-x-[60px] xl:pb-0'>
-          
+      <div className="container mx-auto">
+        <div className="bg-white border-2 border-amber-300 rounded-[50px] min-h-[560px] px-12 pb-2 flex flex-col text-center  xl:flex-row xl:items-center xl:text-left  xl:gap-x-[60px] xl:pb-0">
           {/* image */}
-          <div className='flex-1' data-aos='zoom-in-left'>
-            <img src={image} alt='' />
-          </div>
+          {/* <div className="flex-1" data-aos="zoom-in-left">
+            <img src={image} alt="" />
+          </div> */}
           {/* text */}
-          <div className='flex-1 xl:pr-12'>
-            <h2 className='h2 mb-10' data-aos='fade-up' data-aos-delay='300'>
+          <div className="flex-1 xl:pr-12">
+            <h2 className="h2 mb-10 text-center" >
               {title}
             </h2>
-            <p
-              className='text-justify max-w-[474px] mx-auto xl:mx-0'
-              data-aos='fade-up'
-              data-aos-delay='400'
+            <div
+              className="h4 text-justify  mx-auto xl:mx-0"
             >
-              {subtitle}
-            </p>
+              <p className="mt-4 indent-12 ">{subtitle1}</p>
+              <p className="mt-4 indent-12 ">{subtitle2}</p>
+              <p className="mt-4 indent-12 ">{subtitle3}</p>
+              <p className="mt-4 indent-12 ">{subtitle4}</p>
+            </div>
           </div>
         </div>
       </div>
