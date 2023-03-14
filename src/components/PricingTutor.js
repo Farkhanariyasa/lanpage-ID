b90483e89b58275e93f5b508d1c2b9dce60cee14
Farkhanariyasa <90878131+Farkhanariyasa@users.noreply.github.com>
2023-03-14 22:03:34 +0700

    pricing for desktop

--- src/components/PricingTutor.js
+++ src/components/PricingTutor.js
@@ -2,75 +2,24 @@ import React from "react";
 
 const PricingTutor = () => {
   return (
-    <div className="flex gap-x-[20px] xl:flex-row">
-      <div className="h-auto border-2 border-[#387fbc]  xl:w-[25%]  bg-white transition-all duration-500 transform xl:hover:-translate-y-6 hover:scale-10 shadow-xl hover:shadow-2xl cursor-pointer rounded-[20px] p-8 ">
-        <h2 className="h2 text-center">Basic</h2>
-        <h1 class="text-3xl text-center font-bold">
-          <span class="text-[16px] italic font-normal">Free </span>Rp0.00
-        </h1>
-        <p className="text-center mt-[40px] mb-[75px]">
-          Paket ini cocok untuk kamu yang mau kenalan dulu sama kami dan pengen
-          share permasalahan yang dihadapi di penelitian/skripsi/tesis/disertasi
-          kamu
-        </p>
-        <ul class="text-center">
-          <li>
-            <div class="font-semibold">Free 15 menit meeting</div>
-          </li>
-          <li>
-            <div class="font-semibold">.</div>
-          </li>
-          <li>
-            <div class="font-semibold">.</div>
-            <div class="font-semibold">.</div>
-            <div class="font-semibold mb-8" >.</div>
-          </li>
-        </ul>
-        <div class="text-center">
-          <button
-            class="rounded-2xl h-12  w-48 bg-[#FEE63C] font-bold text-lg text-primary relative overflow-hidden my-4"
-            onClick={() => {
-              window.open(
-                "https://wa.me/62811908400?text=Halo%20Admin%20Saya%20ingin%20order%20paket%20Basic",
-                "_blank"
-              );
-            }}
-          >
-            Pesan Sekarang
-          </button>
-        </div>
-      </div>
-
+    <div className="flex justify-center gap-x-[20px] xl:flex-row">
       <div className="h-auto border-2 border-[#387fbc]  xl:w-[25%]  bg-white transition-all duration-500 transform hover:-translate-y-6 hover:scale-10 shadow-xl hover:shadow-2xl cursor-pointer rounded-[20px] p-8 ">
-        <h2 className="h2 text-center">Ordinary</h2>
+        <h2 className="h2 text-center">Introvert</h2>
         <h1 class="text-3xl text-center font-bold">
-          <span class="text-[16px] italic font-normal">Start </span>
-          Rp499.000
+          <span class="text-[16px] italic font-normal">Mulai dari </span>
+          Rp299.000
         </h1>
-        <p className="text-center mt-[40px] mb-[75px]">
-          Paket ini cocok untuk kamu yang cuma perlu diolahin datanya, dijelasin
-          cara mengerjakannya saja.
-        </p>
+        <div class="p-4 text-center">
+          Paket ini cocok buat kamu yang pengen belajar dan ditutorin one-on-one
+          (private). Pastinya tutor kami profesional, berpengalaman, dan dengan
+          berbagai tipe kepribadian.
+        </div>
         <ul class="text-center">
-          <li>
-            <div class="font-semibold">Running Data</div>
-          </li>
-          <li>
-            <div class="font-semibold">Gratis Interpretasi</div>
-          </li>
-          <li>
-            <div class="font-semibold">
-              Materi Metode Statistik yang digunakan
-            </div>
-          </li>
-          <li>
-            <div class="font-semibold">Free 2x meeting</div>
-          </li>
-          <li>
-            <div class="font-semibold">.</div>
-            <div class="font-semibold mb-8">.</div>
-          </li>
+          <li>&bull; Mau request yang sesama introvert?</li>
+          <li>&bull; Mau yang heboh?</li>
+          <li>&bull; Atau malah yang setengah-setengah?</li>
         </ul>
+        <div class="text-center">Ada semua kok di tim kami!</div>
         <div class="text-center">
           <button
             class="rounded-2xl h-12  w-48 bg-[#FEE63C] font-bold text-lg text-primary relative overflow-hidden my-4"
@@ -86,77 +35,24 @@ const PricingTutor = () => {
         </div>
       </div>
 
-      <div className="h-auto border-2 border-[#387fbc]  xl:w-[25%]  bg-white transition-all duration-500 transform hover:-translate-y-6 hover:scale-10 shadow-xl hover:shadow-2xl cursor-pointer rounded-[20px] p-8 ">
-        <h2 className="h2 text-center">Extraordinary</h2>
-        <h1 class="text-3xl text-center font-bold">
-          <span class="text-[16px] italic font-normal">Start </span>
-          Rp599.000
-        </h1>
-        <p className="text-center mt-[40px] mb-[75px]">
-          Paket ini cocok untuk kamu yang perlu diolahin datanya, dijelasin cara
-          mengerjakannya, plus dibantu menuliskan hasil dan pembahasannya.
-        </p>
-        <ul class="text-center">
-          <li>
-            <div class="font-semibold">Running Data</div>
-          </li>
-          <li>
-            <div class="font-semibold">Gratis Interpretasi</div>
-          </li>
-          <li>
-            <div class="font-semibold">
-              Materi Metode Statistik yang digunakan
-            </div>
-          </li>
-          <li>
-            <div class="font-semibold">Free 2x meeting</div>
-          </li>
-          <li>
-            <div class="font-semibold mb-9">Free 2x revisi minor</div>
-          </li>
-        </ul>
-        <div class="text-center">
-          <button
-            class="rounded-2xl h-12  w-48 bg-[#FEE63C] font-bold text-lg text-primary relative overflow-hidden my-4"
-            onClick={() => {
-              window.open(
-                "https://wa.me/62811908400?text=Halo%20Admin%20Saya%20ingin%20order%20paket%20Extraordinary",
-                "_blank"
-              );
-            }}
-          >
-            Pesan Sekarang
-          </button>
-        </div>
-      </div>
-
       <div className="h-auto border-2 border-[#387fbc]  xl:w-[25%]  bg-white transition-all duration-500 transform hover:-translate-y-6 hover:scale-10 shadow-xl hover:shadow-2xl cursor-pointer rounded-[20px] p-4">
-        <h2 className="h2 text-[32px] mt-3 text-center">Meeting Only</h2>
+        <h2 className="h2 text-[32px] mt-3 text-center">Ekstrovert</h2>
         <h1 class="text-3xl text-center font-bold">
-          <span class="text-[16px] italic font-normal">Only </span>Rp350.000
+          <span class="text-[16px] italic font-normal">Mulai dari </span>
+          Rp 99.000
         </h1>
-        <div class="p-4">
-          <p class="font-bold">Paket ini cocok untuk kamu yang:</p>
-          <ul class="list-disc pl-6">
-            <li>
-              Udah ngolah sendiri data tapi bingung cara baca output dari
-              program
-            </li>
-            <li>
-              Udah ngolah dan interpretasi sendiri tapi gak yakin apakah proses
-              yang udah dilakukan sudah benar atau belum
-            </li>
-            <li>Belum tau sama sekali mau ngolah data apa</li>
-          </ul>
-          <ul class="text-center mt-4">
-            <li>
-              <div class="font-semibold">1x meeting (2 jam)</div>
-            </li>
-          </ul>
+        <h3 className="h3 text-[16px] mt-3 text-center">
+          per orang (min. 3 orang)
+        </h3>
+        <div class="p-4 text-center">
+          Paket ini cocok untuk kamu yang pengen belajar ramean bareng
+          temen-temen. Tenang aja, kami punya tutor yang profesional,
+          berpengalaman dan pastinya asik banget sehingga kamu pasti bisa ngerti
+          materi Statistik dengan sangat mudah!
         </div>
         <div class="text-center">
           <button
-            class="rounded-2xl h-12 w-48 bg-[#FEE63C] font-bold text-lg text-primary relative overflow-hidden my-4"
+            class="rounded-2xl h-12 w-48 bg-[#FEE63C] font-bold text-lg text-primary relative overflow-hidden mt-28"
             onClick={() => {
               window.open(
                 "https://wa.me/62811908400?text=Halo%20Admin%20Saya%20ingin%20order%20paket%20Extraordinary",
